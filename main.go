6af61767a1ca188ab42a01b7e035e73d26fb0201
Jere Lantz <lantz.jere@gmail.com>
2025-04-29 22:28:23 +0300

    http server works

--- main.go
+++ main.go
@@ -1,4 +1,17 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 func main(){
+	handler := http.NewServeMux()
+	server := http.Server{
+		Addr: ":42069",
+		Handler: handler,
+	}
+
+	log.Printf("server started on port %s\n", server.Addr)
+	log.Fatal(server.ListenAndServe())
 }
