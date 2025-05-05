ec40ed3dc3305b3a0cd9076ac1f817165867248d
Jere Lantz <lantz.jere@gmail.com>
2025-05-05 13:38:19 +0300

    database connection established and created the schema for notes and users

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"musiikkiProjektit/handlers"
 	"musiikkiProjektit/views/index"
@@ -19,11 +21,80 @@ func handleServeIndex(w http.ResponseWriter, r *http.Request){
 	index.Index().Render(r.Context(), w)
 }
 
+func dbConnect() (*sql.DB, error){
+	db, err := sql.Open("sqlite3", "database.db")
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func initializeDBSchema(db *sql.DB) error{
+	err := initializeUsersSchema(db)
+	if err != nil {
+		return fmt.Errorf("%s, while creating the users schema", err)
+	}
+
+	err = initializeNotesSchema(db)
+	if err != nil {
+		return fmt.Errorf("%s, while creating the notes schema", err)
+	}
+	return nil
+}
+
+func initializeUsersSchema(db *sql.DB) error{
+	initQuery := `CREATE TABLE IF NOT EXISTS users(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		username text NOT NULL,
+		salt text NOT NULL,
+		password text NOT NULL
+	);`
+
+	_, err := db.Exec(initQuery)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func initializeNotesSchema(db *sql.DB) error{
+	initQuery := `CREATE TABLE IF NOT EXISTS notes(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		title text NOT NULL,
+		note text NOT NULL
+	);`
+
+	_, err := db.Exec(initQuery)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func main(){
 	handler := http.NewServeMux()
 	server := http.Server{
 		Addr: ":42069",
 		Handler: handler,
+	}
+
+	db, err := dbConnect()
+	defer db.Close()
+	if err != nil {
+		log.Fatalf("Error establishing the database connectin: %s\n", err)
+	}
+
+	err = initializeDBSchema(db)
+	if err != nil {
+		log.Fatalf("Error creating the database schema: %s\n", err)
 	}
 
 	// Pages
