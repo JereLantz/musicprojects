58cb83a465c316cd2f470f3541c864f4e1dd2c11
Jere Lantz <lantz.jere@gmail.com>
2025-08-06 15:59:29 +0300

    added the buttons for increasing and decreasing the length of the chord progression

--- public/index.js
+++ public/index.js
@@ -23,3 +23,42 @@ function setSelectorsFlat(){
         select.value = "flat"
     }
 }
+
+/** adds a chord selector to the chord progress page
+ */
+function addChordSelect(){
+    const parent = document.getElementById("chordSelectors")
+
+    const selects = parent?.querySelectorAll("select")
+    if (!selects) return
+
+    if (selects?.length > 8){
+        return
+    }
+
+    const newSel = document.createElement("select")
+    newSel.name = "chordSelect"
+    for (let i = 1; i < 8; i++){
+        const newOption = document.createElement("option")
+        newOption.value = String(i)
+        newOption.innerText = String(i)
+
+        newSel.appendChild(newOption)
+    }
+
+    parent?.appendChild(newSel)
+}
+
+/** removes a chord selector on the chord progress page
+ */
+function removeChordSelect(){
+    const parent = document.getElementById("chordSelectors")
+    if (!parent) return
+
+    const selects = parent?.querySelectorAll("select")
+    if(!selects) return
+
+    if(selects.length > 1) {
+        selects[selects.length - 1].remove()
+    }
+}
